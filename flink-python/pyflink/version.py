a36153ed570f105073de813e4589e01cd6abdf06
ravimagham <ravimagham@lyft.com>
2022-08-05 14:00:28 -0700

    Revert "Commit for release 1.13-devlyft202208051659716885"
    This reverts commit c7401c9e947c923d2a952831a1a1674f2b830b25.

--- flink-python/pyflink/version.py
+++ flink-python/pyflink/version.py
@@ -20,4 +20,4 @@
 The pyflink version will be consistent with the flink version and follow the PEP440.
 .. seealso:: https://www.python.org/dev/peps/pep-0440
 """
-__version__ = "1.13+devlyft202208051659716885"
+__version__ = "1.13.dev0"
